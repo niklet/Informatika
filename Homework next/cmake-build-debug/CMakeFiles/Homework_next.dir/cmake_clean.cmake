95786a96735f76be5ff8cf8dc0d62dbdc2593571
Niklet <nikle002@mail.ru>
2021-02-08 17:06:12 +0300

    1

---
+++ Homework next/cmake-build-debug/CMakeFiles/Homework_next.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_next.dir/main.cpp.obj"
+  "Homework_next.exe"
+  "Homework_next.exe.manifest"
+  "Homework_next.pdb"
+  "libHomework_next.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework_next.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
