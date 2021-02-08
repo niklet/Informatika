35f04384886a12691c5d6bd37bfc831b063491e8
Niklet <nikle002@mail.ru>
2021-02-08 16:57:27 +0300

    Домашняя работа 1

---
+++ Homework 4/cmake-build-debug/CMakeFiles/Homework_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_4.dir/main.cpp.obj"
+  "Homework_4.exe"
+  "Homework_4.exe.manifest"
+  "Homework_4.pdb"
+  "libHomework_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
